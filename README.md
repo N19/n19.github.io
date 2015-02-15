16690fe4f559bc2ecc91efb938f6d34775a21015
N19 <nikolas.rsilva@gmail.com>
2015-02-14 22:16:04 -0200

    remove ALL the files o///

--- README.md
+++
@@ -1,4 +0,0 @@
-N19's Website
-=============
-
-Site pessoal para testes.
--- js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	$('#name').effect('bounce', {times:10}, 2000);
-	$('#name').click(function() {
-		$('#name').effect('bounce', {times:10}, 2000);
-	});
-});

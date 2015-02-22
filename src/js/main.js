85bad7b21a2c48b08028646238bad89ad75c5922
Nikolas Silva <nikolas.rsilva@gmail.com>
2015-02-22 02:47:46 -0300

    mais algumas alterações

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
 // Menu
-var displayMenu = document.getElementById('display-main-menu'),
+var menuButton = document.getElementById('menu-button'),
 	menu = document.querySelector('#main-menu .content'),
 	htmlBody = document.querySelector('html, body');
 
-displayMenu.addEventListener('click', function(e) {
+menuButton.addEventListener('click', function(e) {
 	e.stopPropagation();
 
-	displayMenu.classList.toggle('active');
+	menuButton.classList.toggle('active');
 	menu.classList.toggle('open');
 });
 
 htmlBody.addEventListener('click', function() {
-	displayMenu.classList.remove('active');
+	menuButton.classList.remove('active');
 	menu.classList.remove('open');
 });
